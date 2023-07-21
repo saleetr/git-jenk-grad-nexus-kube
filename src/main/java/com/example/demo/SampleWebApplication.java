9c1d7aaf8f41588e6bfa84b0dbf117b3c0d4989b
saleetr <saleetrana@hotmail.com>
2023-07-21 20:11:01 +0500

    error added

--- src/main/java/com/example/demo/SampleWebApplication.java
+++ src/main/java/com/example/demo/SampleWebApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleWebApplication {
+public class SampleWebApplication {   ASDFASDFASDFASDFASD
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleWebApplication.class, args);
 	}
+
+
+	ASDFASDFASDFA
+		ASDFASDFASDF
+		ASDFASDFASDF
 
 }
